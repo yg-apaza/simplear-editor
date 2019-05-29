fabc5ebf2665b1cab6fccb7095ed07be0ca01071
Yuliana Apaza <yuliana.apaza94@gmail.com>
2019-05-29 00:23:43 -0500

    Setup blocks for Blocky * Create a Blockly service * Initialize Blockly on Editor component

---
+++ src/app/editor/blockly-util.ts
@@ -0,0 +1,130 @@
+declare var Blockly: any;
+
+export class BlocklyUtil {
+
+    toolbox = `
+      <xml xmlns="http://www.w3.org/1999/xhtml" id="toolbox" style="display: none;">
+        <category name="Position" colour="#5b6da5">
+          <block type="position_xyz">
+            <field name="x">0</field>
+            <field name="x">0</field>
+            <field name="x">0</field>
+          </block>
+          <block type="position_center"></block>
+        </category>
+        <category name="Rotation" colour="#5ba56d">
+          <block type="action_rotate"></block>
+          <block type="rotation">
+            <field name="angle">90</field>
+            <field name="axis">x</field>
+            <field name="direction">clock</field>
+          </block>
+        </category>
+      </xml>`;
+
+    constructor() {
+        Blockly.Blocks.component_augment_marker = {
+          init() {
+            this.appendDummyInput()
+                .appendField(new Blockly.FieldImage('https://img.icons8.com/metro/420/settings.png', 15, 15, '*'))
+                .appendField('COMPONENT SETTINGS');
+            this.appendValueInput('NAME')
+                .setCheck('position')
+                .appendField('Initial position');
+            this.appendStatementInput('NAME')
+                .setCheck('action')
+                .setAlign(Blockly.ALIGN_RIGHT)
+                .appendField('On click on resource');
+            this.setInputsInline(false);
+            this.setColour(230);
+            this.setTooltip('');
+            this.setHelpUrl('');
+          }
+        };
+
+        Blockly.Blocks.action_nothing = {
+          init() {
+            this.appendDummyInput()
+                .appendField('Do nothing');
+            this.setPreviousStatement(true, 'action');
+            this.setColour(120);
+            this.setTooltip('');
+            this.setHelpUrl('');
+          }
+        };
+
+        Blockly.Blocks.position_xyz= {
+          init() {
+            this.appendDummyInput()
+                .appendField('X')
+                .appendField(new Blockly.FieldNumber(0), 'x');
+            this.appendDummyInput()
+                .appendField('Y')
+                .appendField(new Blockly.FieldNumber(0), 'x');
+            this.appendDummyInput()
+                .appendField('Z')
+                .appendField(new Blockly.FieldNumber(0), 'x');
+            this.setInputsInline(true);
+            this.setOutput(true, 'position');
+            this.setColour(330);
+            this.setTooltip('');
+            this.setHelpUrl('');
+          }
+        };
+
+        Blockly.Blocks.position_center = {
+          init() {
+            this.appendDummyInput()
+                .appendField('center of marker');
+            this.setOutput(true, 'position');
+            this.setColour(330);
+            this.setTooltip('');
+            this.setHelpUrl('');
+          }
+        };
+
+        Blockly.Blocks.action_rotate = {
+          init() {
+            this.appendValueInput('NAME')
+                .setCheck('rotation')
+                .appendField('Rotate resource');
+            this.setInputsInline(true);
+            this.setPreviousStatement(true, 'action');
+            this.setNextStatement(true, 'action');
+            this.setColour(120);
+            this.setTooltip('');
+            this.setHelpUrl('');
+          }
+        };
+
+        Blockly.Blocks.rotation = {
+          init() {
+            this.appendDummyInput()
+                .appendField(new Blockly.FieldAngle(90), 'angle')
+                .appendField('around')
+                .appendField(new Blockly.FieldDropdown([['X','x'], ['Y','y'], ['Z','z']]), 'axis')
+                .appendField('in direction')
+                .appendField(new Blockly.FieldDropdown([['clockwise','clock'], ['counter clockwise','counterclock']]), 'direction');
+            this.setOutput(true, 'rotation');
+            this.setColour(330);
+            this.setTooltip('');
+            this.setHelpUrl('');
+          }
+        };
+    }
+
+    initialize(blocklyArea: HTMLElement, blocklyDiv: HTMLElement) {
+      const workspace = Blockly.inject(blocklyDiv, { toolbox: this.toolbox });
+      const onresize = () => {
+        blocklyDiv.style.left = '0px';
+        blocklyDiv.style.top = '0px';
+        blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
+        blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
+        Blockly.svgResize(workspace);
+      };
+      window.addEventListener('resize', onresize, false);
+      onresize();
+      Blockly.svgResize(workspace);
+    }
+
+}
--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProjectModel } from '../shared/project/project.model';
-
-declare var Blockly: any;
+import { BlocklyUtil } from './blockly-util';
 
 @Component({
   selector: 'app-editor',
@@ -13,26 +12,7 @@ export class EditorComponent implements OnInit {
 
   project = new ProjectModel('', '', '');
   componentSelected: string;
-
-  toolbox = `
-    <xml xmlns="http://www.w3.org/1999/xhtml" id="toolbox" style="display: none;">
-      <category name="Events">
-        <block type="marker_is_detected">
-          <field name="MARKER_NAME">MARKER</field>
-        </block>
-        <block type="resource_is_selected">
-          <field name="RESOURCE_NAME">RESOURCE</field>
-        </block>
-      </category>
-      <category name="Actions">
-        <block type="augment_resource">
-          <field name="RESOURCE_NAME">RESOURCE</field>
-        </block>
-        <block type="rotate_resource">
-          <field name="RESOURCE_NAME">RESOURCE</field>
-        </block>
-      </category>
-    </xml>`;
+  blocklyUtil = new BlocklyUtil();
 
   constructor(
     private router: Router,
@@ -43,7 +23,7 @@ export class EditorComponent implements OnInit {
     this.activatedRoute.data.subscribe(routeData => {
       if (routeData.project) {
         this.project = routeData.project;
-        this.startBlockly();
+        this.initializeBlockly();
       } else {
         // TODO: Update when resolvers can handle errors
         console.error('Can\'t load this project');
@@ -55,20 +35,10 @@ export class EditorComponent implements OnInit {
     });
   }
 
-  startBlockly() {
+  initializeBlockly() {
     const blocklyArea = document.getElementById('blocklyArea');
     const blocklyDiv = document.getElementById('blocklyDiv');
-    const workspace = Blockly.inject(blocklyDiv, { toolbox: this.toolbox });
-    const onresize = () => {
-      blocklyDiv.style.left = '0px';
-      blocklyDiv.style.top = '0px';
-      blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
-      blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
-      Blockly.svgResize(workspace);
-    };
-    window.addEventListener('resize', onresize, false);
-    onresize();
-    Blockly.svgResize(workspace);
+    this.blocklyUtil.initialize(blocklyArea, blocklyDiv);
   }
 
 }
